c14028e49d7a09d7ad4ae618950a3110333cfa25
Bryon Bailey <bryon.r.bailey@gmail.com>
2026-06-23 23:29:56 -0400

    Update HHM version

--- BetterFishing/BF_Plugin.cs
+++ BetterFishing/BF_Plugin.cs
@@ -15,7 +15,7 @@ namespace BetterFishing
         public const string PLUGIN_VERSION = "1.5.0";
 
         public const string HOOKS_HANG_MORE_GUID = "com.raddude.hookshangmore";
-        public const string HOOKS_HANG_MORE_VERSION = "1.1.0";
+        public const string HOOKS_HANG_MORE_VERSION = "2.0.0";
 
         internal static BF_Plugin Instance { get; private set; }
         private static ManualLogSource _logger;
